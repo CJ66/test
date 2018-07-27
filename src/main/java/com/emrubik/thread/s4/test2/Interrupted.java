808718a122467039c1369fffab1ce75c4769babe
changj <changj@emrubik.com>
2018-07-27 17:02:40 +0800

    itt

---
+++ src/main/java/com/emrubik/thread/s4/test2/Interrupted.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * @(#)Interrupted.java 2018年7月27日
+ *
+ * Copyright 2018 emrubik Group Ltd. All rights reserved.
+ * EMRubik PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+package com.emrubik.thread.s4.test2;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * TODO 这里请补充该类型的简述说明
+ * @author <a href="mailto:changj@emrubik.com">chang jiang</a>
+ * @version $Revision 1.0 $ 2018年7月27日 下午4:52:15
+ */
+public class Interrupted {
+    public static void main(String[] args) throws Exception {
+        // sleepThread不停的尝试睡眠
+        Thread sleepThread = new Thread(new SleepRunner(), "SleepThread");
+        sleepThread.setDaemon(true);
+        // busyThread不停的运行
+        Thread busyThread = new Thread(new BusyRunner(), "BusyThread");
+        busyThread.setDaemon(true);
+        sleepThread.start();
+        busyThread.start();
+        // 休眠5秒，让sleepThread和busyThread充分运行
+        TimeUnit.SECONDS.sleep(5);
+        sleepThread.interrupt();
+        busyThread.interrupt();
+        System.out.println("SleepThread interrupted is " + sleepThread.isInterrupted());
+        System.out.println("BusyThread interrupted is " + busyThread.isInterrupted());
+        // 防止sleepThread和busyThread立刻退出
+        SleepUtils.second(2);
+    }
+
+    static class SleepRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtils.second(10);
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+            }
+        }
+    }
+}
---
+++ src/main/java/com/emrubik/thread/s4/test2/SleepUtils.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * @(#)SleepUtils.java 2018年7月27日
+ *
+ * Copyright 2018 emrubik Group Ltd. All rights reserved.
+ * EMRubik PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+package com.emrubik.thread.s4.test2;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * TODO 这里请补充该类型的简述说明
+ * @author <a href="mailto:changj@emrubik.com">chang jiang</a>
+ * @version $Revision 1.0 $ 2018年7月27日 下午4:30:51
+ */
+public class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
